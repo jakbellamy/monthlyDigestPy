09c9ea8c98e0f6b151bd13df08466de490bc29a4
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-05-11 15:53:46 -0400

    refactored

--- digest_parse.py
+++ digest_parse.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 column_titles = ['Account Name', 'Account Code', 'Account Monthly Quota',
 				'Total Leads Submitted', 'DM Leads Submitted', 'DM Leads Submitted',
@@ -7,6 +8,9 @@ column_titles = ['Account Name', 'Account Code', 'Account Monthly Quota',
 				'Unsubscribe Rate', 'Email Complaints', 'Email Duplicates', 'CASS Failure',
 				'Internal Duplicate', 'Dupe with Prior Batch'
 				]
+
+def persuade_nan(val):
+	return int(val) if math.isnan(val) == False else 0
 
 
 def parse_dfs(counts, locations):
@@ -17,7 +21,7 @@ def parse_dfs(counts, locations):
 		new_row = { # end early in order to self ref
 			'Account Name': location['Account'].values[0] if len(location['Account'].values) > 0 else 'EMPTY VALUE',
 			'Account Code': counts.at[i, 'UserName'],
-			'Account Monthly Quota': location['Quota'].values[0] if len(location['Quota'].values) > 0 else 'EMPTY VALUE',
+			'Account Monthly Quota': location['Quota'].values[0] if len(location['Quota'].values) > 0 else 0,
 			'Total Leads Submitted': int(counts.at[i, 'ArchiveTotal']),
 			'Email Leads Submitted': int(counts.at[i, 'ArchiveEmailTotal']),
 			'Email Duplicates': 'TODO',
@@ -26,12 +30,12 @@ def parse_dfs(counts, locations):
 			'Dupe with Prior Batch': 'TODO'
 		}
 		new_row['DM Leads Submitted'] = new_row['Total Leads Submitted'] - new_row['Email Leads Submitted']
-		new_row['Emails Deployed'] = int(counts.at[i, 'SentTotal']) if type(counts.at[i, 'SentTotal']) == int else 0
-		new_row['Email Opens'] = int(counts.at[i, 'OpenTotal']) if type(counts.at[i, 'OpenTotal']) == int else 0
-		new_row['Email Clicks'] = int(counts.at[i, 'ClickTotal']) if type(counts.at[i, 'ClickTotal']) == int else 0
-		new_row['Email Bounces'] = int(counts.at[i, 'BounceTotal']) if type(counts.at[i, 'BounceTotal']) == int else 0
-		new_row['Unsubscribes'] = int(counts.at[i, 'UnsubTotal']) if type(counts.at[i, 'UnsubTotal']) == int else 0
-		new_row['Email Complaints'] = int(counts.at[i, 'ComplaintTotal']) if type(counts.at[i, 'ComplaintTotal']) == int else 0
+		new_row['Emails Deployed'] = persuade_nan(counts.at[i, 'SentTotal'])
+		new_row['Email Opens'] = persuade_nan(counts.at[i, 'OpenTotal'])
+		new_row['Email Clicks'] = persuade_nan(counts.at[i, 'ClickTotal'])
+		new_row['Email Bounces'] = persuade_nan(counts.at[i, 'BounceTotal'])
+		new_row['Unsubscribes'] = persuade_nan(counts.at[i, 'UnsubTotal'])
+		new_row['Email Complaints'] = persuade_nan(counts.at[i, 'ComplaintTotal'])
 
 		new_df = new_df.append(new_row, ignore_index=True)
 		
