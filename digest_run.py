8a6232e98352090ebfa1bc31c84cb3f9f0bf204f
Jakob <43860560+jakbellamy@users.noreply.github.com>
2020-05-11 11:25:21 -0400

    successfully using the xlsxwriter library in idgest_run

--- digest_run.py
+++ digest_run.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import xlsxwriter
 from digest_parse import df
 
-df.to_excel("output.xlsx")
+writer = pd.ExcelWriter('new_output.xlsx', engine='xlsxwriter')
+
+df.to_excel(writer, sheet_name='April 2020')
+
+writer.save()
